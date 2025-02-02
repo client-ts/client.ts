10746fd0307f183351b55c023648fe783178362a
Shindou Mihou <git@mihou.pw>
2025-02-02 19:04:53 +0800

    Third attempt at resolving types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./builder/createClient";
 export * from "./builder/route";
 export * from "./middlewares/withAuthorization";
+export type * from "./types/builder"
+export type * from "./types/client"
