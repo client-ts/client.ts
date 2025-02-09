0ea90be5b80b0335e122acf807585e4bdd7f76d8
Shindou Mihou <git@mihou.pw>
2025-02-09 13:18:01 +0800

    Increment version to v1.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./builder/client";
 export * from "./builder/route";
 export * from "./builder/hook";
+export * from "./builder/resource";
 export * from "./hooks/usesAuthorization";
 export type * from "./types/builder"
 export type * from "./types/client"
